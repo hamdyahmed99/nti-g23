904fbd274fbbb444ed9ba256e9c9b3e77654d6ee
hamdyahmed99 <hamdyelready@gmail.com>
2022-10-10 11:37:06 +0200

    Update index.js

--- session6/index.js
+++ session6/index.js
@@ -12,6 +12,40 @@ app.use(express.static(staticFiles))
 app.set("view engine", "hbs")
 app.set("views", viewsFiles)
 hbs.registerPartials(layoutsFiles)
+<<<<<<< Updated upstream
 const userRoutes = require("./app/routes/user.routes")
 app.use(userRoutes)
+=======
+app.get("/", (req,res)=>{
+    res.render("home", {
+        pageTitle:"Home Page"
+    })
+
+})
+
+app.get("/addMethod",(req,res)=>{
+    const user ={id:data.now(),...req .query}
+    const allUser =deal.readfrom(JSON)
+})
+app.get("/add", (req,res)=>{
+    res.render("add", {
+        pageTitle:"Add User",
+        title:"Add Ahmed"
+    })
+})
+app.get("/edit", (req,res)=>{
+    res.render("edit", {
+        pageTitle:"Edit User"
+    })
+})
+app.get("/single", (req,res)=>{
+    res.render("single", {
+        pageTitle:"Single User"
+    })
+})
+
+app.get("/delete", (req,res)=>{
+    res.send("delete")
+})
+>>>>>>> Stashed changes
 app.listen(PORT, ()=>console.log(`http://localhost:${PORT}`))
