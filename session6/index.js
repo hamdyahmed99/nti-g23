f8cb21f5f44c44984523881f2302f8fb1000d1c3
marwaMRadwan <marwaradwan872@hotmail.com>
2022-10-10 10:17:13 +0200

    s6

---
+++ session6/index.js
@@ -0,0 +1,41 @@
+//npm init --y
+// npm i express hbs dotenv
+const express = require("express")
+const hbs = require("hbs")
+const path = require("path")
+require("dotenv").config()
+const app = express()
+const PORT = process.env.PORT || 3000
+const staticFiles = path.join(__dirname, "frontend/public")
+const viewsFiles = path.join(__dirname, "frontend/views")
+const layoutsFiles = path.join(__dirname, "frontend/layouts")
+app.use(express.static(staticFiles))
+app.set("view engine", "hbs")
+app.set("views", viewsFiles)
+hbs.registerPartials(layoutsFiles)
+app.get("/", (req,res)=>{
+    res.render("home", {
+        pageTitle:"Home Page"
+    })
+})
+app.get("/add", (req,res)=>{
+    res.render("add", {
+        pageTitle:"Add User",
+        title:"Add Ahmed"
+    })
+})
+app.get("/edit", (req,res)=>{
+    res.render("edit", {
+        pageTitle:"Edit User"
+    })
+})
+app.get("/single", (req,res)=>{
+    res.render("single", {
+        pageTitle:"Single User"
+    })
+})
+
+app.get("/delete", (req,res)=>{
+    res.send("delete")
+})
+app.listen(PORT, ()=>console.log(`http://localhost:${PORT}`))
